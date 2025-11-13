a6c52e85d2dbbcd3ceba0f24358aba54a1046c24
Badal Oraon <btech10738.22@bitmesra.ac.in>
2025-11-13 22:42:31 +0000

    IIFE and control flow

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,11 @@
+// IIFE (Immediately Invoked Function Expression)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
